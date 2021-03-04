d12d493e5f02eea465baa65e1cf9598cd1fa098c
Chad Collins <chad@chadcollins.com>
2021-03-03 23:57:35 -0800

    Append DateTime and File Name to Log File
    Add support for logging by date and time to the log file.

--- xml2js.js
+++ xml2js.js
@@ -1,61 +1,86 @@
 const https = require('https');
 const fs = require('fs');
 const xml2js = require('xml2js');
-const parser = new xml2js.Parser({ attrkey: "ATTR" });
+const parser = new xml2js.Parser({attrkey: "ATTR"});
+const host = "storage.googleapis.com";
+const bucket = "gcp-public-data-landsat";
+const downloadFolder = "downloads";
+const protocol = "https://";
+const logFile = "log.txt";
+const preFix = "LC08/01/001/067";
+const maxKeys = 2;
 
-// reference headers for filtering
-const urlRoot = "https://storage.googleapis.com/gcp-public-data-landsat/";
-let req = https.get(urlRoot+ "?prefix=LC08/01/001/067&max-keys=20", function(res) {
+let req = https.get(protocol + host + "/" + bucket + "?prefix=" + preFix + "&max-keys=" + maxKeys, function(res) {
+
     let data = '';
     res.on('data', function(stream) {
         data += stream;
     });
-    res.on('end', function(){
+
+    res.on('end', function() {
         parser.parseString(data, function(error, data) {
-            if(error === null) {
+            if (error === null) {
                 readElements(data);
-            }
-            else {
+                } else {
                 console.log(error);
             }
         });
     });
+
 });
 
-function readElements(data){
-    
+function readElements(data) {
+
     let results = data.ListBucketResult;
     let contents = results.Contents;
-    let nextMarker = results.NextMarker;
-
-    console.log(nextMarker[0]);
+    // let nextMarker = results.NextMarker;
 
     Object.keys(contents).forEach(function(key) {
-        
+
         let regex = new RegExp('/', 'g');
         let downloadedFile = contents[key].Key[0].replace(regex, '-');
 
-        console.log(downloadedFile);     
-        writeBinaryFile(urlRoot+contents[key].Key[0], downloadedFile, fileDownloaded);
+        writeBinaryFile(protocol + host + "/" + bucket + "/" + contents[key].Key[0], downloadedFile, fileDownloaded);
+    
     });
 
 }
 
-function writeBinaryFile(url, downloadedFile, cb){
+function writeBinaryFile(url, downloadedFile, cb) {
 
-    let downloadFolder = "./downloads/";
-    let file = fs.createWriteStream(downloadFolder + downloadedFile);
+    let file = fs.createWriteStream("./" + downloadFolder + "/" + downloadedFile);
 
     https.get(url, function(response) {
         response.pipe(file);
         file.on('finish', function() {
-        file.close(cb(downloadedFile));
+            file.close(cb(downloadedFile));
         });
     });
 }
 
-function fileDownloaded(downloadedFile){
+function fileDownloaded(downloadedFile) {
 
-    console.log("file " + downloadedFile + " was downloaded");
+    writeToLogFile("./" + downloadFolder + "/"+ logFile, downloadedFile);
+
+}
+
+function writeToLogFile(logFilePathName, downloadedFile){
+
+    let timeStamp = dateTimeStringBuilder();
+    fs.appendFile(logFilePathName, timeStamp + "\t" + downloadedFile + "\r", (err) => {
+ 
+        if (err) throw err;
+ 
+        console.log("File "+ downloadedFile + " written to the log.");
+ 
+    });
+
+}
+
+function dateTimeStringBuilder(){
+
+    let dateTime = new Date();
+    let timeStamp = dateTime.getFullYear() + "-" + Number(dateTime.getMonth()+1) + "-" + dateTime.getDay() + " " + dateTime.getHours() + ":" + dateTime.getMinutes() + ":" + dateTime.getSeconds();
+    return timeStamp;
 
 }
